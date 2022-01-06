004d169d92297e81f8b5d204e824b06539273579
Grejdi Gjura <grejdi@mac-mini>
2022-01-05 23:01:16 -0500

    update connection for rds

--- data_platform/db/__init__.py
+++ data_platform/db/__init__.py
@@ -23,13 +23,23 @@ dbSSL = {} # no config initially
 dbName = os.environ.get('DB_NAME')
 # connection url to database
 dbURL = ''
+
+connectArgs = {}
 # if we are on local, then make some updates
 if os.environ.get('ENV') == 'local':
   # a couple of options for local based on whether user/password is set or not
   if dbUser and dbPassword:
-    dbURL = 'postgresql://{}:{}@{}/{}'.format(dbUser, dbPassword, dbHost, dbName)
+    connectArgs = {
+      'host': dbHost,
+      'user': dbUser,
+      'password': dbPassword,
+      'dbname': dbName
+    }
   else:
-    dbURL = 'postgresql://{}/{}'.format(dbHost, dbName)
+    connectArgs = {
+      'host': dbHost,
+      'dbname': dbName
+    }
 
 # otherwise running on RDS
 else:
@@ -38,15 +48,36 @@ else:
 
   # generate IAM-auth password
   dbPassword = rds.generate_db_auth_token(
-    DBHostname='dataplatform.ccnlslbcr8ut.us-east-1.rds.amazonaws.com',
+    DBHostname='dataplatform.proxy-ccnlslbcr8ut.us-east-1.rds.amazonaws.com',
     Port=dbPort,
     DBUsername=dbUser
   )
 
-  dbURL = 'postgresql+psycopg2://{}:{}@{}/{}?sslmode=require'.format(dbUser, dbPassword.replace('%', '%%'), 'dataplatform.ccnlslbcr8ut.us-east-1.rds.amazonaws.com', dbName)
+  connectArgs = {
+    'host': 'dataplatform.proxy-ccnlslbcr8ut.us-east-1.rds.amazonaws.com',
+    'user': dbUser,
+    'password': dbPassword,
+    'dbname': dbName,
+    'sslmode': 'verify-full',
+    'sslrootcert': '/data_platform/data_platform/db/certs/AmazonRootCA1.pem',
+  }
+
+  # dbURL = 'postgresql+psycopg2://{}:{}@{}/{}?sslmode=verify-full'.format(dbUser, dbPassword.replace('%', '%%'), 'dataplatform.proxy-ccnlslbcr8ut.us-east-1.rds.amazonaws.com', dbName)
 
 # create connection
-dbEngine = create_engine(dbURL)
+dbEngine = create_engine('postgresql+psycopg2://', connect_args=connectArgs)
 
 # database session maker
 dbSession = sessionmaker(dbEngine)
+
+# wget https://s3.amazonaws.com/rds-downloads/rds-ca-2019-root.pem
+# export RDSHOST="dataplatform.ccnlslbcr8ut.us-east-1.rds.amazonaws.com"
+# export PG_USER="iamuser"
+# export PGPASSWORD="$(aws rds generate-db-auth-token --hostname $RDSHOST --port 5432 --region us-east-1 --username $PG_USER )"
+# psql "host=$RDSHOST port=5432 sslmode=verify-full sslrootcert=rds-ca-2019-root.pem dbname=dataplatform user=$PG_USER"
+
+# wget https://www.amazontrust.com/repository/AmazonRootCA1.pem
+# export RDSHOST="dataplatform.proxy-ccnlslbcr8ut.us-east-1.rds.amazonaws.com"
+# export PG_USER="postgres"
+# export PGPASSWORD="$(aws rds generate-db-auth-token --hostname $RDSHOST --port 5432 --region us-east-1 --username $PG_USER )"
+# psql "host=$RDSHOST port=5432 sslmode=verify-full sslrootcert=AmazonRootCA1.pem dbname=dataplatform user=$PG_USER"
