4f01fd3065156d3c8fb2658e73d2270929b029dc
Grejdi Gjura <grejdi@mac-mini>
2022-01-08 21:45:34 -0500

    test out db connection

--- aws/s3/lambdas/process_incoming.py
+++ aws/s3/lambdas/process_incoming.py
@@ -1,11 +1,12 @@
 
+import json
 import logging
 
 from data_platform.lambdas import process_incoming
 
 
 def run(event, context):
-
+  logging.error(json.dumps(event))
   logging.error('LAMBDA')
   process_incoming.run()
 
--- data_platform/lambdas/process_incoming.py
+++ data_platform/lambdas/process_incoming.py
@@ -3,10 +3,18 @@ import logging
 from logging_tree import printout
 from logging_tree.format import build_description
 
+from data_platform.db import dbSession
+from data_platform.db.models.prefix import Prefix
+from data_platform.db.models.load import Load
+
 
 def run():
   logging.error('LAMBDA:: PROCESS INCOMING')
   logging.error(build_description())
+
+  with dbSession() as db:
+    # get all load records in 'ready' status
+    loadRecs = db.query(Load, Prefix).join(Prefix, Load.prefix_id == Prefix.id).filter(Load.status == 'ready').order_by(Load.s3_key).all()
 
 
 if __name__ == '__main__':
