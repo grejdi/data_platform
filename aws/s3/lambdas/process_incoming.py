9083a797b2ad34f6d0f08ab04d5fd0388d3d1505
Grejdi Gjura <grejdi@mac-mini>
2022-02-02 15:42:56 -0500

    some logging

--- aws/s3/lambdas/process_incoming.py
+++ aws/s3/lambdas/process_incoming.py
@@ -6,6 +6,7 @@ from data_platform.lambdas import process_incoming
 
 
 def run(event, context):
+  logging.error(json.dumps(event))
   return process_incoming.run(event.get('detail', {}).get('requestParameters', {}).get('key'))
 
 if __name__ == '__main__':
